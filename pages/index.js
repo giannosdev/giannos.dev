392bbad9727545bc3e97ef373af2c65cd7d31b07
giannos <john.char.dev@gmail.com>
2022-02-03 23:39:48 +0200

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return <>
+      <Head>
+          <title>giannos.dev - coming soon</title>
+      </Head>
+      <main className={styles.main}>
+          <h2>coming soon</h2>
+      </main>
+  </>;
+}
